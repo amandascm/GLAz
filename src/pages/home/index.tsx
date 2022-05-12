382ae9323098bd201e7b7def082156c5d184b700
Amanda Moraes <amandascm23@gmail.com>
2022-05-12 09:36:43 -0300

    Update Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo-dark.svg';
+import Navbar from '../../components/Navbar';
+import pages from '../../utils/links';
+import Button from 'react-bootstrap//Button';
+
 import './styles.css';
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="Home">
+      <Navbar links={pages} />
+      <section className="presentation-section">
+        <div className="presentation">
+          <img src={logo} className="logo" id="GLAz" alt="logo" />
+          <h1>A GitHub Languages Analyzer.</h1>
+          <div className="presentation-button-div">
+            <Button variant="dark">Visualizations</Button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
