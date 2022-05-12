2700a444f73a8cb87c709be64588d9016d6ad967
Amanda Moraes <amandascm23@gmail.com>
2022-05-12 02:44:39 -0300

    Add bootstrap script to index.tsx (so we can use their components)

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './styles/index.css';
 import Home from './pages/home';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
